c6194bc222422c42828be6da58a97e47a974f380
Mikołaj Wojtas <vengrath97@gmail.com>
2022-01-15 17:44:08 +0100

    Simplifications

--- TaskOne.cs
+++ TaskOne.cs
@@ -10,31 +10,20 @@ namespace Kolokwium2
     {
         public static bool isPerfect(int input)
         {
-            List<int> denominators = new List<int>();
             int sumofdenominators = 0;
             for (int i = 1; i < input; i++)
             {
-                int change = input % i;
-                if (change == 0) { denominators.Add(i); }
-            }
-            foreach (int item in denominators)
-            {
-                sumofdenominators += item;
+                if (input % i == 0) { sumofdenominators += i; }
             }
             return (sumofdenominators == input) ? true : false;
         }
-        public static List<int> perfectInRange(int input)
+        public static void perfectInRange(int input)
         {
             List<int> listOfPerfectInRange = new List<int>();
             for (int i = 6; i < input; i++) //6, bo 6 to najmniejsza "doskonała liczba, nie ma sensu sprawdzać niżej
             {
-                if (isPerfect(i)) listOfPerfectInRange.Add(i);
+                if (isPerfect(i)) Console.WriteLine(i);
             }
-            foreach (int item in listOfPerfectInRange)
-            {
-                Console.WriteLine(item);
-            }
-            return listOfPerfectInRange;
         }
     }
 }
